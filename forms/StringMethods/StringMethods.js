18d528ee3e8f066faa7578caef7ad0ace726875f
alexsemler <alex.semler03@gmail.com>
2019-09-29 17:39:02 -0500

    Initial commit

---
+++ forms/StringMethods/StringMethods.js
@@ -0,0 +1,15 @@
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+
+let upperCaseString = quoteString.toUpperCase(quoteString)
+
+let authorString = "Henry Ford"
+
+let completeString = quoteString + " " + authorString
+
+let secondQuote = "Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King"
+
+let lowerCaseString = secondQuote.toLowerCase()
+
+let thirdPos = secondQuote.charAt(3)
+
+let findString = 
